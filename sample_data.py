accf0b6969ec16ff361a07e3b49e7f1ffc05672a
Kira <bebrikira@ukr.net>
2026-07-11 15:38:52 +0300

    Create sample_data.py

---
+++ sample_data.py
@@ -0,0 +1,9 @@
+from drone import Drone
+
+fleet_data = [
+    Drone("DJI Mini 4", 92, "Idle"),
+    Drone("Autel EVO Lite", 65, "Idle"),
+    Drone("Parrot Anafi", 18, "Charging"),
+    Drone("DJI Air 3", 54, "Idle"),
+    Drone("Skydio X2", 25, "Maintenance"),
+]
